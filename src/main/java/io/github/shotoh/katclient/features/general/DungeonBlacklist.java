805f332713b5568a966ff2365056307eefe07b93
shotoh <trueshotoh@gmail.com>
2024-01-19 13:50:06 -0800

    implemented dungeon blacklist

--- src/main/java/io/github/shotoh/katclient/features/general/DungeonBlacklist.java
+++ src/main/java/io/github/shotoh/katclient/features/general/DungeonBlacklist.java
@@ -2,35 +2,119 @@ package io.github.shotoh.katclient.features.general;
 
 import io.github.shotoh.katclient.KatClient;
 import io.github.shotoh.katclient.core.KatClientConfig;
+import io.github.shotoh.katclient.utils.Utils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.world.World;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.UUID;
 
 public class DungeonBlacklist {
     @SubscribeEvent
     public void onClientChatReceivedEvent(ClientChatReceivedEvent event) {
         Minecraft mc = Minecraft.getMinecraft();
         EntityPlayerSP player = mc.thePlayer;
+        WorldClient world = mc.theWorld;
         Connection conn = KatClient.DB;
-        if (KatClientConfig.dungeonBlacklist && player != null && conn != null) {
+        if (conn == null) {
+            Utils.sendMessage("§c[KC] Database connection does not exist");
+            return;
+        }
+        if (KatClientConfig.dungeonBlacklist && player != null && world != null) {
             String text = event.message.getUnformattedText();
             if (!text.contains("joined the dungeon group")) return;
             String[] args = text.split(" ");
             if (args.length < 4) return;
-            String user = args[3];
-
+            String name = args[3];
+            UUID uuid = Utils.getUUID(name);
+            if (uuid == null) return;
             try {
                 PreparedStatement statement = conn.prepareStatement(
-                        "SELECT * FROM dungeon_blacklist WHERE username=?");
+                        "SELECT * FROM dungeon_blacklist WHERE uuid=UUID_TO_BIN(?)");
+                statement.setString(1, uuid.toString());
+                ResultSet rs = statement.executeQuery();
+                if (rs.next()) {
+                    String reason = rs.getString("reason");
+                    if (reason != null) {
+                        player.sendChatMessage("/p kick " + name);
+                        Utils.sendMessage("§c[KC] " + name + " is §4blacklisted§c, reason='" + reason + "'");
+                    }
+                }
+                rs.close();
+                statement.close();
             } catch (SQLException e) {
-                player.addChatMessage(new ChatComponentText("§cDatabase query failed"));
+                Utils.sendMessage("§c[KC] Database query failed");
             }
+        }
+    }
+
+    public static void addPlayer(String name, String reason) {
+        if (name == null || reason == null) {
+            Utils.sendMessage("§c[KC] Username or reason is invalid");
+        }
+        Connection conn = KatClient.DB;
+        if (conn == null) {
+            Utils.sendMessage("§c[KC] Database connection does not exist");
+            return;
+        }
+        UUID uuid = Utils.getUUID(name);
+        if (uuid == null) {
+            Utils.sendMessage("§c[KC] Invalid player");
+            return;
+        }
+        try {
+            PreparedStatement statement = conn.prepareStatement(
+                    "INSERT INTO dungeon_blacklist (uuid, username, reason) " +
+                            "VALUES UUID_TO_BIN(?), ?, ?");
+            statement.setString(1, uuid.toString());
+            statement.setString(2, name);
+            statement.setString(3, reason);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                Utils.sendMessage("§c[KC] " + name + " is now §4blacklisted");
+            }
+            rs.close();
+            statement.close();
+        } catch (SQLException e) {
+            Utils.sendMessage("§c[KC] Database query failed (maybe duplicate uuid)");
+        }
+    }
+
+    public static void removePlayer(String name) {
+        if (name == null) {
+            Utils.sendMessage("§c[KC] Username is invalid");
+        }
+        Connection conn = KatClient.DB;
+        if (conn == null) {
+            Utils.sendMessage("§c[KC] Database connection does not exist");
+            return;
+        }
+        UUID uuid = Utils.getUUID(name);
+        if (uuid == null) {
+            Utils.sendMessage("§c[KC] Invalid player");
+            return;
+        }
+        try {
+            PreparedStatement statement = conn.prepareStatement(
+                    "DELETE FROM dungeon_blacklist WHERE uuid=UUID_TO_BIN(?)");
+            statement.setString(1, uuid.toString());
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                Utils.sendMessage("§c[KC] " + name + " is no longer §4blacklisted");
+            }
+            rs.close();
+            statement.close();
+        } catch (SQLException e) {
+            Utils.sendMessage("§c[KC] Database query failed (maybe invalid uuid)");
         }
     }
 }
